8dfb4cb3d5b9c6c2ef68f49476da995ed3431c7c
nuralamsiddque <nurasiddque@gmail.com>
2023-03-29 11:39:37 +0600

    initial

---
+++ assets/js/custom.js
@@ -0,0 +1,28 @@
+jQuery(document).ready(function() {
+    jQuery('ul li a').click(function() {
+        jQuery('li a').removeClass("active");
+        jQuery(this).addClass("active");
+    });
+});
+jQuery(document).ready(function() {
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (function() {
+        'use strict'
+
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.querySelectorAll('.needs-validation')
+
+        // Loop over them and prevent submission
+        Array.prototype.slice.call(forms)
+            .forEach(function(form) {
+                form.addEventListener('submit', function(event) {
+                    if (!form.checkValidity()) {
+                        event.preventDefault()
+                        event.stopPropagation()
+                    }
+
+                    form.classList.add('was-validated')
+                }, false)
+            })
+    })()
+});
